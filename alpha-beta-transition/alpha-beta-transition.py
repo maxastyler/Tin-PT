59b9109e05dde1f96c2485b2cea42c9d93ebd258
Max Tyler <mtyler88@gmail.com>
2018-01-31 11:39:55 +0000

    Added tin transition

--- alpha-beta-transition/alpha-beta-transition.py
+++ alpha-beta-transition/alpha-beta-transition.py
@@ -19,6 +19,25 @@ ts_alpha = sorted(temps_alpha.keys())
 es_alpha = [temps_alpha[i] for i in ts_alpha]
 ts_beta = sorted(temps_beta.keys())
 es_beta = [temps_beta[i] for i in ts_beta]
-plt.plot(ts_beta, es_beta)
-plt.plot(ts_alpha, es_alpha)
+crossing_i = 0
+for i in range(len(ts_beta)):
+    if es_beta[i]<es_alpha[i]:
+        crossing_i = i
+        break
+a_x1 = ts_alpha[i-1]
+a_x2 = ts_alpha[i]
+b_x1 = ts_beta[i-1]
+b_x2 = ts_beta[i]
+a_y1 = es_alpha[i-1]
+a_y2 = es_alpha[i]
+b_y1 = es_beta[i-1]
+b_y2 = es_beta[i]
+a_m = (a_y2 - a_y1)/(a_x2-a_x1)
+b_m = (b_y2 - b_y1)/(b_x2-b_x1)
+a_c = a_y1 - a_m*a_x1
+b_c = b_y1 - b_m*b_x1
+cross_x = (a_c-b_c)/(b_m-a_m)
+print("The crossing temperature is: {:2g}K".format(cross_x))
+plt.plot(ts_beta, es_beta, 'red')
+plt.plot(ts_alpha, es_alpha, 'blue')
 plt.show()
